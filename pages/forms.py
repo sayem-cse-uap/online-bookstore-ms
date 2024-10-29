39fa554ab8c5dfdc1f6be7a2dd5ae364e542e512
sayem-cse-uap <sayem.cse.uap@gmail.com>
2024-10-29 23:21:47 +0600

    validators

--- pages/forms.py
+++ pages/forms.py
@@ -4,6 +4,7 @@ from django import forms
 from django.forms.widgets import PasswordInput, TextInput
 
 from django.contrib.auth.models import User
+# from . models import Customer
 
 class CreateUserForm(UserCreationForm):
   
