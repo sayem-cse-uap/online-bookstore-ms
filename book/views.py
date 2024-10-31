383d679d31191f154793d2f3d74dc24cddee498d
sayem-cse-uap <sayem.cse.uap@gmail.com>
2024-10-31 22:25:42 +0600

    search

--- book/views.py
+++ book/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
+def search(request):
+
+  if request.method == 'POST':
+    searchInput = request.POST['searchInput']
+    books = Book.objects.filter(title__contains=searchInput)
+
+    context = {
+      'searchInput':searchInput,
+      'books':books
+    }
+
+    return render(request, 'search.html', context)
+  else:
+    return render(request, 'search.html')
--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('books/', include('book.urls')),
 ]
 
 if settings.DEBUG:
